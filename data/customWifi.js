d4d93993ae267689f3b3d5ee736dc6679a0f3383
Victor Ng <victor@crankycoder.com>
2015-08-10 17:23:57 -0400

    minimal hookups of xpcom working using port.emit(), port.on() pairs

--- data/customWifi.js
+++ data/customWifi.js
@@ -1,60 +1,10 @@
 var salutation = "hello, ";
+
 function greetme(user) {
-      window.alert(salutation + user);
+    self.port.emit("check_chrome_bits", "blah blah");
+    window.alert(salutation + user);
 }
 
-function doWifiScan() {
-    // TODO: implement the nsIWifiListener interface, register
-    // against the WifiScanner and then initiate a scan to
-    // grab BSSIDs.  Note that some scans (the majority
-    // apparently) will yield empty BSSID lists.  The gecko
-    // cached location code does something reasonably smartish
-    // here.
 
-    var count = 0;
-
-    function test() {
-    }
-
-    test.prototype =
-    {
-        onChange: function (accessPoints)
-        {
-            netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
-
-            console.log("Start access point list....");
-            var macList = "";
-            for (var i=0; i < accessPoints.length; i++) {
-                var a = accessPoints[i];
-                console.log("Got MAC: ["+a.mac+"]");
-                macList += " " + a.mac;
-            }
-            console.log("End access point list....");
-            alert("Got mac addresses : ["+macList+"]");
-        },
-
-        onError: function (value) {
-                     alert("error: " +value);
-                 },
-
-        QueryInterface: function(iid) {
-                            netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
-                            if (iid.equals(Components.interfaces.nsIWifiListener) ||
-                                    iid.equals(Components.interfaces.nsISupports)) {
-                                return this;
-                            }
-                            throw Components.results.NS_ERROR_NO_INTERFACE;
-                        },
-    }
-
-    netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
-
-    var listener = new test();
-    var wifi_service = Components.classes["@mozilla.org/wifi/monitor;1"].getService(Components.interfaces.nsIWifiMonitor);
-
-    wifi_service.startWatching(listener);
-    console.log("wifi monitor is hooked!");
-
-}
  
 exportFunction(greetme, unsafeWindow, {defineAs: "greetme"});
--- index.js
+++ index.js
@@ -1,11 +1,88 @@
+var self = require("sdk/self");
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
-pageMod.PageMod({
-      include: "*",
-      contentScriptWhen: "start",
-      contentScriptFile: [data.url("geo-script.js"),
-                          data.url("customWifi.js")],
-      contentScriptOptions: {
-          showOptions: true
-      }
+
+var {Cc, Ci, Cu, Cr, Cm, components} = require("chrome");
+
+var page = pageMod.PageMod({
+    include: "*",
+    contentScriptWhen: "start",
+    contentScriptFile: [data.url("geo-script.js"),
+    data.url("customWifi.js")],
+    contentScriptOptions: {
+        showOptions: true
+    },
+    onAttach: function(worker) {
+
+                  worker.port.on("check_chrome_bits", function(addonMessage) {
+                      console.log("Addon received message: ["+addonMessage+"]");
+
+                      function showLoadedModules() {
+                          console.log("Loaded some chrome components: " + Cc);
+                          console.log("Loaded some chrome components: " + Ci);
+                          console.log("Loaded some chrome components: " + Cu);
+                          console.log("Loaded some chrome components: " + Cr);
+                          console.log("Loaded some chrome components: " + Cm);
+                          console.log("Loaded some chrome components: " + components);
+                      }
+
+                      showLoadedModules();
+                  });
+              }
 });
+
+
+
+function doWifiScan() {
+    // TODO: implement the nsIWifiListener interface, register
+    // against the WifiScanner and then initiate a scan to
+    // grab BSSIDs.  Note that some scans (the majority
+    // apparently) will yield empty BSSID lists.  The gecko
+    // cached location code does something reasonably smartish
+    // here.
+
+    var count = 0;
+
+    function test() {
+    }
+
+    test.prototype =
+    {
+        onChange: function (accessPoints)
+        {
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+
+            console.log("Start access point list....");
+            var macList = "";
+            for (var i=0; i < accessPoints.length; i++) {
+                var a = accessPoints[i];
+                console.log("Got MAC: ["+a.mac+"]");
+                macList += " " + a.mac;
+            }
+            console.log("End access point list....");
+            alert("Got mac addresses : ["+macList+"]");
+        },
+
+        onError: function (value) {
+                     alert("error: " +value);
+                 },
+
+        QueryInterface: function(iid) {
+                            netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+                            if (iid.equals(Components.interfaces.nsIWifiListener) ||
+                                    iid.equals(Components.interfaces.nsISupports)) {
+                                return this;
+                            }
+                            throw Components.results.NS_ERROR_NO_INTERFACE;
+                        },
+    }
+
+    netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+
+    var listener = new test();
+    var wifi_service = Components.classes["@mozilla.org/wifi/monitor;1"].getService(Components.interfaces.nsIWifiMonitor);
+
+    wifi_service.startWatching(listener);
+    console.log("wifi monitor is hooked!");
+
+}
